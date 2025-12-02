64ec11dce797f85990be42ce45a85ed58f0fb475
Ben Windsor <ben.windsor@skyrad.co.uk>
2025-12-02 10:45:47 +0000

    Day2 Part2

--- day2/day2.py
+++ day2/day2.py
@@ -4,6 +4,7 @@ https://adventofcode.com/2025/day/2
 import pathlib
 import math
 
+
 def day2_part1_algorithm(ranges_str: str) -> int:
     ranges = [[int(x) for x in r.split("-")] for r in ranges_str.split(",")]
 
@@ -46,13 +47,91 @@ def day2_part1_algorithm(ranges_str: str) -> int:
     return total
 
 
+def _get_candidate(num_repeats: int, repeated_part_start: int, d: int):
+    candidate = 0
+    for p in range(0, num_repeats):
+        candidate += repeated_part_start * d ** p
+    return candidate
+
+
+def day2_part2_algorithm(ranges_str: str) -> int:
+    ranges = [[int(x) for x in r.split("-")] for r in ranges_str.split(",")]
+    return day2_part2_algorithm_ranges(ranges)
+
+
+def is_prime(x: int) -> bool:
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+
+def has_repeats(x: int, x_len: int) -> bool:
+    for i_num_repeats in range(2, x_len + 1):
+        if x_len % i_num_repeats == 0 and is_prime(i_num_repeats):
+            d = 10 ** (x_len // i_num_repeats)
+            repeated_part = x // (d ** (i_num_repeats - 1))
+            candidate = _get_candidate(i_num_repeats, repeated_part, d)
+            if x == candidate:
+                return True
+    return False
+
+
+def day2_part2_algorithm_ranges(ranges: list[list[int]]) -> int:
+    total = 0
+    for r_start, r_end in ranges:
+        start_len = math.ceil(math.log10(r_start + 1))
+        end_len = math.ceil(math.log10(r_end + 1))
+
+        # Go through each length of numbers in the range
+        for i_len in range(start_len, end_len+1):
+            x_start = max(r_start, 10 ** (i_len - 1))
+            x_end = min(r_end, 10 ** i_len - 1)
+
+            # Go through each possible number of repeats
+            for i_num_repeats in range(2, i_len+1):
+                if i_len % i_num_repeats == 0:
+                    repeat_len = i_len // i_num_repeats
+                    d = 10**(i_len // i_num_repeats)
+                    repeated_part_start = x_start // (d**(i_num_repeats-1))
+                    repeated_part_end = x_end // (d**(i_num_repeats-1))
+
+                    # Special cases
+                    if repeated_part_start == repeated_part_end and not has_repeats(repeated_part_start, repeat_len):
+                        candidate = _get_candidate(i_num_repeats, repeated_part_start, d)
+                        if x_start <= candidate <= x_end:
+                            total += candidate
+                        continue
+
+                    # If we are here, repeated_part_end > repeated_part_start
+                    # Initial case
+                    if not has_repeats(repeated_part_start, repeat_len):
+                        candidate = _get_candidate(i_num_repeats, repeated_part_start, d)
+                        if candidate >= x_start:
+                            total += candidate
+
+                    # Intermediate cases
+                    for repeated_part in range(repeated_part_start+1, repeated_part_end):
+                        if not has_repeats(repeated_part, repeat_len):
+                            candidate = _get_candidate(i_num_repeats, repeated_part, d)
+                            total += candidate
+
+                    # Final case
+                    if not has_repeats(repeated_part_end, repeat_len):
+                        candidate = _get_candidate(i_num_repeats, repeated_part_end, d)
+                        if candidate <= x_end:
+                            total += candidate
+
+    return total
+
+
 def main():
     this_folder = pathlib.Path(__file__)
     input_file = this_folder.parent / 'input.txt'
 
     with open(input_file, 'r') as f:
         data = f.read().strip()
-    result = day2_part1_algorithm(data)
+    result = day2_part2_algorithm(data)
 
     return result
 
--- test/test_day2.py
+++ test/test_day2.py
@@ -1,8 +1,8 @@
 import pytest
-from day2.day2 import day2_part1_algorithm
+from day2.day2 import day2_part1_algorithm, day2_part2_algorithm
 
 
-def dumb_algorithm(input_str: str) -> int:
+def dumb_algorithm_part1(input_str: str) -> int:
     total = 0
     ranges = [[int(x) for x in r.split("-")] for r in input_str.split(",")]
     for r_start, r_end in ranges:
@@ -15,11 +15,11 @@ def dumb_algorithm(input_str: str) -> int:
     return total
 
 
-def test_day2_full():
+def test_day2_part1_full():
     test_input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
     expected_output = 1227775554
     assert day2_part1_algorithm(test_input) == expected_output
-    assert dumb_algorithm(test_input) == expected_output
+    assert dumb_algorithm_part1(test_input) == expected_output
 
 
 @pytest.mark.parametrize("input_str, expected_output", [
@@ -45,12 +45,40 @@ def test_day2_full():
     ("4-18", 11),
     ("1-30", 11+22),
 ])
-def test_day2(input_str: str, expected_output: int):
+def test_day2_part1(input_str: str, expected_output: int):
     assert day2_part1_algorithm(input_str) == expected_output
-    assert dumb_algorithm(input_str) == expected_output
+    assert dumb_algorithm_part1(input_str) == expected_output
 
 
-def test_find_failures():
+def test_find_failures_part1():
     for x in range(1000):
         input_str = f"1-{x}"
-        assert day2_part1_algorithm(input_str) == dumb_algorithm(input_str), f"Failed for x = {x}"
+        assert day2_part1_algorithm(input_str) == dumb_algorithm_part1(input_str), f"Failed for x = {x}"
+
+
+def test_day2_part2_full():
+    test_input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+    expected_output = 4174379265
+    assert day2_part2_algorithm(test_input) == expected_output
+
+
+@pytest.mark.parametrize("input_str, expected_output", [
+    ("11-22", 33),
+    ("95-115", 99+111),
+    ("998-1012", 999+1010),
+    ("1188511880-1188511890", 1188511885),
+    ("222220-222224", 222222),
+    ("222222-222222", 222222),
+    ("212121212121-212121212121", 212121212121),
+    ("212121-212121", 212121),
+    ("22222-22222", 22222),
+    ("1698522-1698528", 0),
+    ("446443-446449", 446446),
+    ("38593856-38593862", 38593859),
+    ("565653-565659", 565656),
+    ("824824821-824824827", 824824824),
+    ("2121212118-2121212124", 2121212121),
+    ("68686868-68686868", 68686868),
+])
+def test_day2_part1(input_str: str, expected_output: int):
+    assert day2_part2_algorithm(input_str) == expected_output
