2ba3f6cd4e51e676c387ad198432ae165b3ddd0b
Ben Windsor <ben.windsor@skyrad.co.uk>
2025-12-06 09:55:37 +0000

    Day6 Part1

---
+++ day6/day6.py
@@ -0,0 +1,106 @@
+"""
+https://adventofcode.com/2025/day/6
+"""
+import pathlib
+import os
+from io import BufferedReader
+import re
+
+OPERATOR_MULTIPLY = 0
+OPERATOR_ADD = 1
+
+
+def get_line_len(f: BufferedReader):
+    """Looks backwards from the end of a file for the first \n character and returns the length of the final line
+    including the \n at the end of that line"""
+
+    f.seek(0, os.SEEK_END)
+    # File pointer is now at the end of the file
+    # Now find the length of a line
+    try:
+        f.seek(-1024, os.SEEK_CUR)
+        data = f.read(1024)
+    except OSError:  # Tried to move before start of file
+        f.seek(0, os.SEEK_SET)
+        data = f.read()
+
+    data = data.rstrip(b'\n')  # Remove newlines from the end of the file
+    newline_pos = data.rfind(b'\n')
+    if newline_pos >= 0:
+        line_len = len(
+            data) - newline_pos  # (-1 for correctnesss, +1 to include newline at end which we stripped, cancels out)
+    else:
+        line_len = len(data) + 1
+        while True:
+            cur_pos = f.tell()
+            try:
+                f.seek(-2048, os.SEEK_CUR)
+                data = f.read(1024)
+            except OSError:  # Tried to move before start of file
+                f.seek(0, os.SEEK_SET)
+                data = f.read(cur_pos)
+            newline_pos = data.rfind(b'\n')
+            if newline_pos >= 0:
+                line_len += len(data) - newline_pos - 1
+                break
+            else:
+                line_len += len(data)
+
+    return line_len
+
+
+def column_iterator(f: BufferedReader, start_pos: int, line_len: int):
+    f.seek(start_pos, os.SEEK_SET)
+    data = f.read(1024)  # May return less than 1024 bytes if we are at the end of the file
+    offset = 0
+    while len(data) > 0:
+        end_idx = 0
+        for match in re.finditer(br'[\+\*] +', data):
+            yield match.start() + offset, match.end() - match.start(), OPERATOR_MULTIPLY if b'*' in match.group(0) else OPERATOR_ADD
+            end_idx = match.end()
+        new_data = f.read(1024)
+        if len(new_data) == 0:
+            break
+        offset += end_idx
+        data = data[end_idx:] + new_data
+
+
+def day6_part1_algorithm(input_filename: str) -> int:
+    with open(input_filename, 'rb') as f:
+        line_len = get_line_len(f)
+
+        f.seek(0, os.SEEK_END)
+        file_len = f.tell()
+        num_lines = file_len // line_len
+
+        result = 0
+        with open(input_filename, 'rb') as f_op:
+            for i_col, col_width, operator in column_iterator(f_op, line_len * (num_lines - 1), line_len):
+                part_result = 0 if operator == OPERATOR_ADD else 1
+                for i_row in range(0, num_lines - 1):
+                    f.seek(i_row * line_len + i_col, os.SEEK_SET)
+                    data = int(f.read(col_width))
+                    if operator == OPERATOR_ADD:
+                        part_result += data
+                    else:
+                        part_result *= data
+                result += part_result
+
+    return result
+
+
+def day6_part2_algorithm(input_filename: str) -> int:
+    pass
+
+
+def main():
+    this_folder = pathlib.Path(__file__)
+    input_file = this_folder.parent / 'input.txt'
+
+    result = day6_part1_algorithm(str(input_file))
+
+    return result
+
+
+if __name__ == "__main__":
+    print(main())
---
+++ test/test_day6.py
@@ -0,0 +1,48 @@
+import pytest
+from day6.day6 import day6_part1_algorithm, day6_part2_algorithm, get_line_len
+import tempfile
+import pathlib
+
+
+@pytest.mark.parametrize("line_len", [
+    1, 100, 1023, 1024, 1025, 2047, 2048, 2049, 10000
+])
+def test_get_line_len(line_len: int):
+    with tempfile.TemporaryDirectory() as td:
+        with open(pathlib.Path(td) / "input.txt", "wb") as f:
+            f.write(b"0" * (line_len - 1))
+            f.write(b'\n')
+            f.write(b"1" * (line_len - 1))
+            f.write(b'\n\n')
+        with open(pathlib.Path(td) / "input.txt", "rb") as f:
+            assert get_line_len(f) == line_len
+
+
+def test_day6_part1_full():
+    expected_output = 4277556
+    with tempfile.TemporaryDirectory() as td:
+        with open(pathlib.Path(td) / "input.txt", "wb") as f:
+            f.write(b"123 328  51 64 \n")
+            f.write(b" 45 64  387 23 \n")
+            f.write(b"  6 98  215 314\n")
+            f.write(b"*   +   *   +  \n\n")
+        assert day6_part1_algorithm(str(pathlib.Path(td) / "input.txt")) == expected_output
+
+
+def test_day6_part1():
+    expected_output = 668 + 8161 + 592
+    with tempfile.TemporaryDirectory() as td:
+        with open(pathlib.Path(td) / "input.txt", "wb") as f:
+            f.write(b"668 8161 592\n")
+            f.write(b"+   +    +  \n\n")
+        assert day6_part1_algorithm(str(pathlib.Path(td) / "input.txt")) == expected_output
+
+
+def test_day3_part2_full():
+    return
+    test_input = """123 328  51 64 
+     45 64  387 23 
+      6 98  215 314
+    *   +   *   +  """
+    expected_output = 3121910778619
+    assert day6_part2_algorithm(test_input) == expected_output
